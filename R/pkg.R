f7de5debf2ae2158474a5c0b3b90dce33ce49d80
taylor <taylor@taylors-MacBook.local>
2017-04-22 23:18:10 +0100

    fixing typos

---
+++ R/pkg.R
@@ -0,0 +1,12 @@
+#' tif: Text Interchange Formats
+#'
+#' This package describes and validates formats for storing
+#' common object arising in text analysis as native R objects.
+#' Representations of a text corpus, document term matrix, and
+#' tokenized text are included. The tokenized text format is
+#' extensible to include other annotations.
+#'
+#' @import Matrix
+#'
+#' @docType package
+"_PACKAGE"
